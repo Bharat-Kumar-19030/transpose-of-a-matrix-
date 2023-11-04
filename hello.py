c75f87f6df11c30f32788553819a402c7cda44bf
Bharat Kumar <149879338+Bharat-Kumar-19030@users.noreply.github.com>
2023-11-04 19:03:35 +0530

    Create hello.py
    hello messege

---
+++ hello.py
@@ -0,0 +1,5 @@
+a=input("Enter Your Name: ")
+b=int(Enter Your Age: ")
+c=int(input("In Which class are you in? Enter in numbers like 1,2,3.. :")
+print("Hello Dear",a,"How are you ? Hope you are happy and healthy")
+
