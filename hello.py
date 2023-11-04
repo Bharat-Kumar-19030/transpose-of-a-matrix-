aedabb76932b145106a27f6e5de6bf1ecdb4b32e
Bharat Kumar <149879338+Bharat-Kumar-19030@users.noreply.github.com>
2023-11-04 19:12:36 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,4 +2,10 @@ a=input("Enter Your Name: ")
 b=int(Enter Your Age: ")
 c=int(input("In Which class are you in? Enter in numbers like 1,2,3.. :")
 print("Hello Dear",a,"How are you ? Hope you are happy and healthy")
-
+if 13=<b<=19:
+      print("Your are a teenager")
+      for i in range(b):
+      if i<10:
+      print("you are still in",c,"class
+      
+      
